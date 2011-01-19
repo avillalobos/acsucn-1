e775c43808d60e9137de12675bd12b951d1b6c1f
a.e.v.r.007 <a.e.v.r.007@d7a85929-a13b-5166-4859-8762c7bf5a63>
2011-01-19 15:51:26 +0000

    Guardando cambios

--- acs/internship/2010/avillalobos/src/JavaClient.java
+++ acs/internship/2010/avillalobos/src/JavaClient.java
@@ -1,6 +1,31 @@
 
-public class JavaClient {
-	public static void main(String args[]){
-		
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+import alma.acs.container.ContainerServicesBase;
+import alma.JavaContainerError.*;
+import alma.acs.component.client.ComponentClient;
+import alma.acs.container.corba.AcsCorba;
+
+public class JavaClient extends ComponentClient {
+
+	private  JavaClient jc;
+	
+	protected JavaClient(Logger logger, String managerLoc, String clientName,
+			AcsCorba externalAcsCorba) throws Exception {
+		super(logger, managerLoc, clientName, externalAcsCorba);
+		// TODO Auto-generated constructor stub
 	}
+	
+	public void doSomeStuff() throws AcsJContainerServicesEx{
+        this.jc = (JavaClient) getContainerServices().getComponent("Meteorologic");
+	}
+
+
+	public static void main(String args[]){
+		System.out.println("Hello world");
+	}
+	
+	
 }
