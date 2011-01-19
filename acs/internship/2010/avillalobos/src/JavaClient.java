b0b84a2b6ad66d89f83b3d0a723aa284d4092e00
a.e.v.r.007 <a.e.v.r.007@d7a85929-a13b-5166-4859-8762c7bf5a63>
2011-01-19 18:52:43 +0000

    Guardando cambios

--- acs/internship/2010/avillalobos/src/JavaClient.java
+++ acs/internship/2010/avillalobos/src/JavaClient.java
@@ -2,8 +2,19 @@
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
+/*
+import alma.maciErrType.ComponentNotAlreadyActivatedEx;
+import alma.acs.util.IsoDateFormat;
+import alma.ACSErrTypeCommon.wrappers.AcsJIllegalArgumentEx;
+import org.exolab.castor.core.exceptions.CastorException;
+import alma.cdbErrType.CDBXMLErrorEx;
+import org.omg.DsLogAdmin.BasicLogOperations;
+import alma.Logging.AcsLogServiceOperations;
+import alma.maci.loggingconfig.UnnamedLogger;
+import si.ijs.maci.ClientPOA;
+import alma.acs.logging.AcsLogger;
 import alma.acs.container.ContainerServicesBase;
+*/
 import alma.JavaContainerError.wrappers.AcsJContainerServicesEx;
 import alma.acs.component.client.ComponentClient;
 import alma.acs.container.corba.AcsCorba;
@@ -12,6 +23,7 @@ public class JavaClient extends ComponentClient {
 
 	private  JavaClient jc;
 	
+	// Constructor of JavaClient using only info for ComponentClient
 	protected JavaClient(Logger logger, String managerLoc, String clientName,
 			AcsCorba externalAcsCorba) throws Exception {
 		super(logger, managerLoc, clientName, externalAcsCorba);
@@ -20,11 +32,17 @@ public class JavaClient extends ComponentClient {
 	
 	public void doSomeStuff() throws AcsJContainerServicesEx{
         this.jc = (JavaClient) getContainerServices().getComponent("Meteorologic");
+        System.out.println("Obteniendo el componente Meteorologic");
 	}
 
 
-	public static void main(String args[]){
+	public static void main(String args[]) throws Exception{
 		System.out.println("Hello world");
+		String managerLoc = System.getProperty("ACS.manager");
+		System.out.println("managerloc = " + managerLoc);
+		String clientName = "Meteorologic";
+		JavaClient javaclient = new JavaClient(null,managerLoc,clientName, null);
+		javaclient.doSomeStuff();
 	}
 	
 	
