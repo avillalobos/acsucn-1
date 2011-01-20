a632bb24d8e4b22f99ff117f846d16f9c036f5cf
a.e.v.r.007 <a.e.v.r.007@d7a85929-a13b-5166-4859-8762c7bf5a63>
2011-01-20 14:24:37 +0000

    doSomeStuff ahora retorna un valor double

--- acs/internship/2010/avillalobos/src/JavaClient.java
+++ acs/internship/2010/avillalobos/src/JavaClient.java
@@ -6,6 +6,7 @@ import alma.JavaContainerError.wrappers.AcsJContainerServicesEx;
 import alma.acs.component.client.ComponentClient;
 import alma.acs.container.corba.AcsCorba;
 
+
 public class JavaClient extends ComponentClient {
 
 	private  Meteorologic jc;
@@ -16,23 +17,23 @@ public class JavaClient extends ComponentClient {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void doSomeStuff() throws AcsJContainerServicesEx{
+	public double doSomeStuff() throws AcsJContainerServicesEx{
 		// This is the name of .xml that define the .idl
 		this.jc = MeteorologicHelper.narrow(getContainerServices().getComponentNonSticky("Meteo"));
 		this.jc.displayMessage("y desde el cliente te digo, componente CTM!");
 		ROdouble t = this.jc.temperature();
 		System.out.println("la temperatura en el OSF es de " + t.default_value());
         System.out.println("Obteniendo el componente Meteorologic");
+        return t.default_value();
 	}
 
 
 	public static void main(String args[]) throws Exception{
-		System.out.println("Hello world");
 		//String managerLoc = System.getProperty("ACS.manager");
 		String managerLoc = "corbaloc::10.195.17.114:3000/Manager";
 		System.out.println("managerloc = " + managerLoc);
 		String clientName = "Meteorologic";
 		JavaClient javaclient = new JavaClient(null,managerLoc,clientName, null);
-		javaclient.doSomeStuff();
+		double d = javaclient.doSomeStuff();
 	}
 }
