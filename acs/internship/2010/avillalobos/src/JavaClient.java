d96959d1da78590903478446d97b8afc4bc79c81
a.e.v.r.007 <a.e.v.r.007@d7a85929-a13b-5166-4859-8762c7bf5a63>
2011-01-21 12:56:57 +0000

    Agregue el metodo getStateFromComponent y logginFINEmessage y la obtencion generica de componentes para el status

--- acs/internship/2010/avillalobos/src/JavaClient.java
+++ acs/internship/2010/avillalobos/src/JavaClient.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
 import alma.ACS.ROdouble;
@@ -6,6 +8,9 @@ import alma.JavaContainerError.wrappers.AcsJContainerServicesEx;
 import alma.acs.component.client.ComponentClient;
 import alma.acs.container.corba.AcsCorba;
 
+import alma.ACS.ACSComponent;
+import alma.ACS.ACSComponentHelper;
+import alma.ACS.ComponentStates;
 
 public class JavaClient extends ComponentClient {
 
@@ -20,22 +25,48 @@ public class JavaClient extends ComponentClient {
 	public double doSomeStuff() throws AcsJContainerServicesEx{
 		// This is the name of .xml that define the .idl
 		this.jc = MeteorologicHelper.narrow(getContainerServices().getComponent("Meteo"));
-		System.out.println("El nombre del container es " + this.getContainerServices().getName());
+		ACSComponent acscomponent = ACSComponentHelper.narrow(this.jc);
+		ComponentStates state = acscomponent.componentState();
+		System.out.println("El nombre del comnponente es " + this.getContainerServices().getName() + " y el stado del componente es " + state.toString());
 		this.jc.displayMessage("y desde el cliente te digo, componente CTM!");
 		ROdouble t = this.jc.temperature();
 		System.out.println("la temperatura en el OSF es de " + t.default_value());
         System.out.println("Obteniendo el componente Meteorologic");
         return t.default_value();
 	}
-
+	
+	public void loggerFINEmessage(String msg){
+		this.m_logger.fine(msg);
+	}
+	
+	public String getStateFromComponent(String name){
+		try {
+			ACSComponent acscomponent = ACSComponentHelper.narrow(this.getContainerServices().getComponent(name));
+			return acscomponent.componentState().toString();
+		} catch (AcsJContainerServicesEx e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			return "Component Doesn't Exist";
+		}
+	}
 
 	public static void main(String args[]) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//String managerLoc = System.getProperty("ACS.manager");
 		String managerLoc = "corbaloc::10.195.17.114:3000/Manager";
 		System.out.println("managerloc = " + managerLoc);
-		String clientName = "Meteorologic";
+		String clientName = "ACS Generci Java Client";
 		JavaClient javaclient = new JavaClient(null,managerLoc,clientName, null);
 		double d = javaclient.doSomeStuff();
 		System.out.println("La temperatura retornada es de " + d);
+		System.out.println("Ingrese el nombre del componente");
+		String componentName = br.readLine();
+		while (!componentName.equals("exit")){
+			System.out.println("El stado del componente es " + javaclient.getStateFromComponent(componentName));
+			System.out.println("Ingrese el nombre del componente");
+			componentName = br.readLine();
+		}
+		javaclient.loggerFINEmessage("Exiting from java Client");
 	}
 }
