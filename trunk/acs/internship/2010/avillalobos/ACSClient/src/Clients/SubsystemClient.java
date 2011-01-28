bd598720863cf1a5f031d916e10a0e0b3ba0c02e
a.e.v.r.007 <a.e.v.r.007@d7a85929-a13b-5166-4859-8762c7bf5a63>
2011-01-28 20:08:14 +0000

    Agregando formato json al retorno

--- trunk/acs/internship/2010/avillalobos/ACSClient/src/Clients/SubsystemClient.java
+++ trunk/acs/internship/2010/avillalobos/ACSClient/src/Clients/SubsystemClient.java
@@ -38,31 +38,25 @@ public class SubsystemClient extends ACSClient{
 	public LinkedList<String> getStatus() {
 		LinkedList<String> report = new LinkedList<String>();
 		org.omg.CORBA.Object ref = null;
-		
 		MasterComponent masterComp = null;
-		for(int i = 0; i < this.Subsystems.length; i++){
+		for(String subsystem : this.Subsystems){
 			try {
-				ref = getContainerServices().getComponent(this.Subsystems[i]);
+				ref = getContainerServices().getComponent(subsystem);
 				masterComp = MasterComponentHelper.narrow(ref);
-				report.add("Estado actual del subsistema  " + this.Subsystems[i] + " = " + masterComp.componentState().toString());
+				alma.ACS.ROstringSeq baciProperty = masterComp.currentStateHierarchy();
+				alma.ACSErr.Completion compl = new Completion(System.currentTimeMillis(), 0, 0, new alma.ACSErr.ErrorTrace[]{});
+				alma.ACSErr.CompletionHolder complHolder = new CompletionHolder(compl);
+				String[] status = baciProperty.get_sync(complHolder);
+				report.add("{Type:'Subsystem',Name:'"+subsystem+"',CompStat:'"+masterComp.componentState().toString()+"',SubSysStat:'"+status[1]+"'}");
+				//report.add("Estado actual del subsistema  " + this.Subsystems[i] + " = " + masterComp.componentState().toString());
 			} catch (AcsJContainerServicesEx e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-				report.add("No se puede obtener la informacion del subsistema " +this.Subsystems[i]);
+				report.add("No se puede obtener la informacion del subsistema " +subsystem);
 			} catch (BAD_OPERATION e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-				report.add("No se puede obtener la informacion del subsistema " +this.Subsystems[i]);
-			}
-			//System.out.println("Cantidad de caracteristicas" + masterComp.get_all_characteristics().get_number_of_properties());
-			//System.out.println("Estado actual del subsistema  " + masterComp.componentState().toString());
-			alma.ACS.ROstringSeq baciProperty = masterComp.currentStateHierarchy();
-			alma.ACSErr.Completion compl = new Completion(System.currentTimeMillis(), 0, 0, new alma.ACSErr.ErrorTrace[]{});
-			alma.ACSErr.CompletionHolder complHolder = new CompletionHolder(compl);
-			String[] status = baciProperty.get_sync(complHolder);
-			System.out.println("Imprimiendo status");
-			for(int j =0 ; j < status.length; j++){
-				System.out.println(status[i]);
+				report.add("No se puede obtener la informacion del subsistema " +subsystem);
 			}
 		}
 		
